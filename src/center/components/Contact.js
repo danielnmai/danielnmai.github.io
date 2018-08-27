d4c2e097d6bb8fd4cf954962be18a21d7ecb8142
danielnmai <danielmai1728@gmail.com>
2018-08-27 10:04:13 -0700

    test state settings

--- src/center/components/Contact.js
+++ src/center/components/Contact.js
@@ -8,19 +8,29 @@ import '../styles/contact.css';
 @observer
 class Contact extends Component {
   @observable formInput = null;
-  
+
   constructor(props) {
     super(props)
     this.formInput = { formSubmitted: false, name: '', email: '', message: '' }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = { date: new Date() }
   }
-  
+  componentDidMount(){
+    this.timerId = setInterval(() => this.tick(), 1000)
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+
   handleSubmit(event) {
     event.preventDefault()
     console.log('form is submitted')
     this.formInput.formSubmitted = true
     console.log(this.formInput)
-    
+
   }
   handleNameChange = (event) => {
     this.formInput.name = event.target.value
@@ -31,26 +41,27 @@ class Contact extends Component {
   handleMessageChange = (event) => {
     this.formInput.message = event.target.value
   }
-  
-  
+
+
   render() {
     return (
       <div id='contact' className='contact'>
-      <h1>Contact</h1>       
-      <div className='body'>
-      <form onSubmit={this.handleSubmit}>            
-      <input className='form-input' type='text' name='name' placeholder="Name" value={this.formInput.name} onChange={ this.handleNameChange} />
-      <input className='form-input' type='text' name='email' placeholder="Email" value={this.formInput.email} onChange={this.handleEmailChange} />
-      <textarea className='form-input' type='text' name='message' placeholder="Message" value={this.formInput.message} onChange={ this.handleMessageChange} />
-      <input className='form-input' id='submit-button' type='submit' value='Send Message' />
-      </form>
-      <div className='info'>
-      <h3>Daniel Mai</h3>
-      <p> Milpitas, California</p>
-      <p> danielmai1728@gmail.com</p>
-      {/* {this.formInput.formSubmitted && <FormOutput {...this.formInput} />}         */}
-      </div>
-      </div>
+        <h1>Contact</h1>
+        <div className='body'>
+          <form onSubmit={this.handleSubmit}>
+            <input className='form-input' type='text' name='name' placeholder="Name" value={this.formInput.name} onChange={this.handleNameChange} />
+            <input className='form-input' type='text' name='email' placeholder="Email" value={this.formInput.email} onChange={this.handleEmailChange} />
+            <textarea className='form-input' type='text' name='message' placeholder="Message" value={this.formInput.message} onChange={this.handleMessageChange} />
+            <input className='form-input' id='submit-button' type='submit' value='Send Message' />
+          </form>
+          <div className='info'>
+            <h3>Daniel Mai</h3>
+            <p> Milpitas, California</p>
+            <p> danielmai1728@gmail.com</p>
+            <div><p>{this.state.date.toLocaleTimeString()}</p></div>
+            <p>{this.timerId} </p>
+          </div>
+        </div>
       </div>
     )
   }
@@ -59,12 +70,12 @@ class Contact extends Component {
 const FormOutput = (props) => {
   return (
     <div>
-    <div>Form Output: </div>
-    <p>name: {props.name}</p>
-    <p>email: {props.email}</p>
-    <p>message: {props.message}</p>
+      <div>Form Output: </div>
+      <p>name: {props.name}</p>
+      <p>email: {props.email}</p>
+      <p>message: {props.message}</p>
     </div>
-    
+
   )
 }
 
