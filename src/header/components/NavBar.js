11bad36d269be2adaa359f3b83297ae80855f989
danielnmai <danielmai1728@gmail.com>
2018-09-29 10:42:39 -0700

    modify navbar classname to avoid conflict with bootstrap

--- src/header/components/NavBar.js
+++ src/header/components/NavBar.js
@@ -5,7 +5,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 class NavBar extends React.Component {
   render(){
     return (
-      <div className='navbar'>
+      <div className='nav'>
         <a href='#home'>Home</a>
         <a href='#project'>Projects</a>
         <a href='#about'>About</a>
