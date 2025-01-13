dde5a0e6ba3f581425d6a089a6db3fe01ac2cf85
Daniel Mai <danielmai1728@gmail.com>
2025-01-12 17:59:17 -0800

    show more or less text in a testimonial

--- src/components/TestimonialItem.vue
+++ src/components/TestimonialItem.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const isHidden = ref(false);
+const isHidden = ref(true);
+const showMoreText = () => (isHidden.value = false);
+
+const showLessText = () => (isHidden.value = true);
 </script>
 
 <template>
-  <div class="flex-row">
+  <div class="flex flex-col">
     <slot name="content"></slot>
-    <slot name="more-content"></slot>
-    <div class="justify-self-end">
-      <span>Show More</span>
+    <div v-show="!isHidden">
+      <slot name="more-content"></slot>
+    </div>
+    <div class="self-center m-5">
+      <a @click="isHidden ? showMoreText() : showLessText()"
+        >Show {{ isHidden ? "More" : "Less" }}
+      </a>
     </div>
   </div>
 </template>
