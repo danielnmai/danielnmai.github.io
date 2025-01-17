3e740064ff598685ba19a6ad22e07792337b0c53
Daniel Mai <danielmai1728@gmail.com>
2025-01-17 14:30:14 -0800

    add responsive design for testimonials

--- src/components/TestimonialComponent.vue
+++ src/components/TestimonialComponent.vue
@@ -7,24 +7,41 @@ const props = defineProps<{
   imageRightSide?: boolean;
 }>();
 
+import { onUnmounted, ref } from "vue";
+
+const useInnerWidth = () => {
+  const width = ref(window.innerWidth);
+  const syncWidth = () => (width.value = window.innerWidth);
+  window.addEventListener("resize", syncWidth);
+  onUnmounted(() => window.removeEventListener("resize", syncWidth));
+
+  return width;
+};
+
+const width = useInnerWidth();
+console.log("width", width);
+
 const getImageURL = () =>
   new URL(`../assets/pictures/${props.imageName}`, import.meta.url).toString();
 </script>
 
 <template>
-  <div class="flex flex-col my-5">
+  <div class="my-5">
     <div v-if="!imageRightSide">
-      <div class="flex flex-row leading-normal">
+      <div class="flex flex-col md:flex-row leading-normal">
         <div class="flex flex-col mt-5">
           <img
             :alt="name"
-            class="md:max-w-[200px] object-contain rounded-full mr-5"
+            class="md:max-w-[200px] max-w-[300px] object-contain rounded-full mr-5 self-center md:self-start"
             :src="getImageURL()"
           />
-          <a class="self-center font-bold mt-5" :href="url" target="_blank">{{
-            name
-          }}</a>
-          <span class="text-center">{{ title }}</span>
+          <a
+            class="self-center md:self-start font-bold mt-5"
+            :href="url"
+            target="_blank"
+            >{{ name }}</a
+          >
+          <span class="text-center md:text-left">{{ title }}</span>
         </div>
         <div class="pl-5 mt-5 bg-lightGray p-5 rounded-3xl">
           <v-icon
@@ -38,27 +55,56 @@ const getImageURL = () =>
       </div>
     </div>
 
-    <div v-if="imageRightSide">
-      <div class="flex flex-row leading-normal">
-        <div class="pl-5 mt-5 bg-lightGray p-5 rounded-3xl">
-          <v-icon
-            class="my-2"
-            name="fa-quote-left"
-            scale="2.0"
-            fill="#166534"
-          />
-          <slot name="content"></slot>
+    <div v-else>
+      <div v-if="width < 768">
+        <div class="flex flex-col md:flex-row leading-normal">
+          <div class="flex flex-col mt-5">
+            <img
+              :alt="name"
+              class="md:max-w-[200px] max-w-[300px] object-contain rounded-full mr-5 self-center md:self-start"
+              :src="getImageURL()"
+            />
+            <a
+              class="self-center md:self-start font-bold mt-5"
+              :href="url"
+              target="_blank"
+              >{{ name }}</a
+            >
+            <span class="text-center md:text-left">{{ title }}</span>
+          </div>
+          <div class="pl-5 mt-5 bg-lightGray p-5 rounded-3xl">
+            <v-icon
+              class="my-2"
+              name="fa-quote-left"
+              scale="2.0"
+              fill="#166534"
+            />
+            <slot name="content"></slot>
+          </div>
         </div>
-        <div class="flex flex-col mt-5 ml-5">
-          <img
-            :alt="name"
-            class="md:max-w-[200px] object-contain rounded-full mr-5"
-            :src="getImageURL()"
-          />
-          <a class="self-center font-bold mt-5" :href="url" target="_blank">{{
-            name
-          }}</a>
-          <span class="text-center">{{ title }}</span>
+      </div>
+      <div v-else>
+        <div class="flex flex-col md:flex-row sm:flex-col leading-normal">
+          <div class="pl-5 mt-5 bg-lightGray p-5 rounded-3xl">
+            <v-icon
+              class="my-2"
+              name="fa-quote-left"
+              scale="2.0"
+              fill="#166534"
+            />
+            <slot name="content"></slot>
+          </div>
+          <div class="flex flex-col mt-5 ml-5">
+            <img
+              :alt="name"
+              class="md:max-w-[200px] max-w-[300px] object-contain rounded-full mr-5 self-center md:self-start"
+              :src="getImageURL()"
+            />
+            <a class="self-center font-bold mt-5" :href="url" target="_blank">{{
+              name
+            }}</a>
+            <span class="text-center">{{ title }}</span>
+          </div>
         </div>
       </div>
     </div>
